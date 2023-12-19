973c06d213ed9a8053dde8c8deed3f40fade3dad
Daniel Thom <daniel.thom@nrel.gov>
2023-12-19 08:49:50 -0700

    Fix license

--- src/infra_sys/__about__.py
+++ src/infra_sys/__about__.py
@@ -1,4 +1,1 @@
-# SPDX-FileCopyrightText: 2023-present Daniel Thom <daniel.thom@nrel.gov>
-#
-# SPDX-License-Identifier: BSD-3-Clause
 __version__ = "0.0.1"
--- src/infra_sys/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present Daniel Thom <daniel.thom@nrel.gov>
-#
-# SPDX-License-Identifier: BSD-3-Clause
