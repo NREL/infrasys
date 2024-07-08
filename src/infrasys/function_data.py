392944bf2b6a06408f4121346efa0179936608db
Jerry Potts <jerry.potts@utexas.edu>
2024-07-08 13:55:07 -0600

    Added IncrementalCurve conversions

--- src/infrasys/function_data.py
+++ src/infrasys/function_data.py
@@ -195,3 +195,22 @@ def get_slopes(vc: List[XYCoords]) -> List[float]:
         slopes.append((comp_y - prev_y) / (comp_x - prev_x))
         (prev_x, prev_y) = (comp_x, comp_y)
     return slopes
+
+
+def get_x_lengths(x_coords: List[float]) -> List[float]:
+    return np.subtract(x_coords[1:], x_coords[:-1])
+
+
+def running_sum(data: PiecewiseStepData) -> List[XYCoords]:
+    points = []
+    slopes = data.y_coords
+    x_coords = data.x_coords
+    x_lengths = get_x_lengths(x_coords)
+    running_y = 0.0
+
+    points.append(XYCoords(x=x_coords[0], y=running_y))
+    for prev_slope, this_x, dx in zip(slopes, x_coords[1:], x_lengths):
+        running_y += prev_slope * dx
+        points.append(XYCoords(x=this_x, y=running_y))
+
+    return points
